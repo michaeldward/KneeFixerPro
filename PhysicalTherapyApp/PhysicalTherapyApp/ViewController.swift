2628898b725e6e38de1df4f06ea20b3946f57e71
mikethebro <xenophon.of.athens@gmail.com>
2017-04-12 16:26:08 -0600

    adding database functionality

--- PhysicalTherapyApp/PhysicalTherapyApp/ViewController.swift
+++ PhysicalTherapyApp/PhysicalTherapyApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -15,8 +16,11 @@ class ViewController: UIViewController {
     //let exercises = ["Knee flexion", "Knee extension", "Heel slides", "Hamstring stretch", "Isometric quadriceps exercise", "Step ups", "Leg lifts", "Shuttle runs", "Figure 8's", "Suicides", "Single-leg balances", "Wobble-board", "Step downs", "Stationary biking", "Half-squat", "Partial-lunges", "Heel raises"]
     @IBOutlet weak var viewTable: UITableView!
     
+    var ref: FIRDatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        FIRApp.configure()
         self.title = "Exercises"
         exerciseList.addElement(newElement: Exercise(title: "Knee flexion", description: "Initiate sagittal plane plyometrics, work towards single leg plyometrics.Clearance by MD and pass Sportsmetric training before returning to full athletics. NOTE: All progressions are approximations and should be used as a guideline only. Progression will be based on individual patient presentation, which is assessed throughout the treatment process.", assigned: false, index: 0))
         exerciseList.addElement(newElement: Exercise(title: "Knee extension", description: "Rehabilitation for the injured knee begins immediately following ACL injury. The clinical goals for Phase I include restoring full range of motion (ROM) and normal strength and control swelling prior to surgery. Patients are also to completely understand the basic principles of accelerated rehabilitation including full terminal knee extension, early weight bearing, and closed and open chain strengthening. The time needed to accomplish these goals can be as little as 1 week or as long as 2 months, depending on how the knee responds to the initial injury.", assigned: false, index: 1))
@@ -25,6 +29,8 @@ class ViewController: UIViewController {
         exerciseList.addElement(newElement: Exercise(title: "Half-Squat", description: "Stand holding a sturdy table with both hands. With your feet placed shoulder’s width apart, slowly bend your knees and squat, lowering your hips into a half-squat. Hold 10 seconds and then slowly return to a standing position. Repeat 10 times.", assigned: false, index: 3))
         exerciseList.addElement(newElement: Exercise(title: "Partial Lunges", description: "Stand holding a sturdy table with both hands. With your feet placed shoulder’s width apart, take a half step forward, keeping your weight evenly distributed. Slowly bend your knees and sink down slightly. Hold 10 seconds and then slowly return to a standing position. Repeat on the other side. Do 10 times per side.", assigned: false, index: 4))
         exerciseList.addElement(newElement: Exercise(title: "Heel Raises", description: "While standing, place your hand on a counter or back of a chair for balance. Raise up onto your toes and hold for five seconds. Slowly lower your heel to the floor and repeat 10 times.", assigned: false, index: 5))
+        //get exercises from Firebase database
+        ref = FIRDatabase.database().reference(withPath: "exercises")
         
         // Do any additional setup after loading the view, typically from a nib.
     }
